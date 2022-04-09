02e4aa9673b26218b189729ab6be7288c44737f8
WilsonHWS159 <n96101189@gs.ncku.edu.tw>
2022-04-09 18:18:47 +0800

    Add len int cmp.v and rewrite LZ77_Encoder.v

--- HW3/LZ77_Encoder.v
+++ HW3/LZ77_Encoder.v
@@ -12,16 +12,12 @@ output reg	[2:0] 	match_len;
 output reg 	[7:0] 	char_nxt;
 
 // 0~7 is look-ahead buffer, 8-16 is search buffer
-reg [16:0] buff[7:0];
-reg [3:0] slide_cnt, s_cnt, l_cnt, hold;
-reg [8:0] cmp[7:0];
-wire l_full, s_full;
+reg [7:0] buff[16:0];
+reg [3:0] hold;
+reg [16:0] buff_BM;
 
 always @(posedge reset) begin
-	s_cnt <= 0;
-	l_cnt <= 0;
-	slide_cnt <= 0;
-	
+	buff_BM <= 17'h00000;
 	valid <= 1'b0;
 	encode <= 1'b1;
 	finish <= 1'b0;
@@ -34,29 +30,66 @@ end
 integer i;
 always @(posedge clk) begin
 	buff[0] <= chardata;
-	for(i=0; i<16; i=i+1)
+	buff_BM[0] <= 1'b1;
+	for (i=0; i<16; i=i+1) begin
 		buff[i+1] <= buff[i];
-end
-
-assign l_full = l_cnt == 4'h8;
-assign s_full = s_cnt == 4'h9;
-
-always @(posedge clk) begin
-	if (l_full) begin
-		l_cnt <= l_cnt;
-		if (s_full)
-			s_cnt <= s_cnt;
-		else
-			s_cnt <= s_cnt + 1;
-	end else
-		l_cnt <= l_cnt + 1;
-end
-
-always @(posedge clk) begin
-	if (l_full & !hold) begin
-	end else begin
+		buff_BM[i+1] <= buff_BM[i];
 	end
 end
 
+reg [7:0] cmp_result[8:0];
+reg [7:0] v_result;
+
+generate
+	genvar j;
+	for (j=0; j<9; j=j+1) begin: CMP
+		cmp Cmp(.buff1({buff[16-j], buff[15-j], buff[14-j], buff[13-j], buff[12-j], buff[11-j], buff[10-j], buff[9-j]}), .buff2({buff[7], buff[6], buff[5], buff[4], buff[3], buff[2], buff[1], buff[0]}), .result(cmp_result[8-j]));
+	end
+endgenerate
+/*
+always @(posedge clk) begin
+	if (!hold & buff_BM[7]) begin
+		hold <= hold;
+		valid <= 1'b1;
+		if (buff_BM[16] & ) begin
+			offset <= 4'h8;
+			v_result <= cmp_result[8];
+		end else if (buff_BM[15] & ) begin
+			offset <= 4'h7;
+			v_result <= cmp_result[7];
+		end else if (buff_BM[14] & ) begin
+			offset <= 4'h6;
+			v_result <= cmp_result[6];
+		end else if (buff_BM[13] & ) begin
+			offset <= 4'h5;
+			v_result <= cmp_result[5];
+		end else if (buff_BM[12] & ) begin
+			offset <= 4'h4;
+			v_result <= cmp_result[4];
+		end else if (buff_BM[11] & ) begin
+			offset <= 4'h3;
+			v_result <= cmp_result[3];
+		end else if (buff_BM[10] & ) begin
+			offset <= 4'h2;
+			v_result <= cmp_result[2];
+		end else if (buff_BM[9] & ) begin
+			offset <= 4'h1;
+			v_result <= cmp_result[1];
+		end else if (buff_BM[8] & ) begin
+			offset <= 4'h0;
+			v_result <= cmp_result[0];
+		end else begin
+			offset <= 4'h0;
+			v_result <= 8'hFF;
+		end
+	end else if (~buff_BM[7]) begin
+		valid <= 1'b0;
+		offset <= 4'h0;
+		match_len <= 3'h0;
+		char_nxt <= 8'h00;
+	end else
+		hold <= hold - 1;
+end
+*/
 endmodule
 
--- HW3/cmp.v
+++ HW3/cmp.v
@@ -1,7 +1,8 @@
-module cmp(buff1, buff2, result);
+module cmp(buff1, buff2, result, len);
 input		[63:0]	buff1;
 input 		[63:0]	buff2;
 output reg	[7:0]	result;
+output reg	[4:0]	len;
 
 always @(*) begin
 	result[0] = |(buff1[7:0] - buff2[7:0]);
@@ -12,6 +13,20 @@ always @(*) begin
 	result[5] = |(buff1[47:40] - buff2[47:40]);
 	result[6] = |(buff1[55:48] - buff2[55:48]);
 	result[7] = |(buff1[63:56] - buff2[63:56]);
+end
+
+always @(*) begin
+	casez (result)
+		default: len = 5'h00;
+		8'b00000000: len = 5'h8;
+		8'b00000001: len = 5'h7;
+		8'b0000001z: len = 5'h6;
+		8'b000001zz: len = 5'h5;
+		8'b00001zzz: len = 5'h4;
+		8'b0001zzzz: len = 5'h3;
+		8'b001zzzzz: len = 5'h2;
+		8'b01zzzzzz: len = 5'h1;
+	endcase
 end
 
 endmodule

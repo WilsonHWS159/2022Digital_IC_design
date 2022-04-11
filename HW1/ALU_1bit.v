9c67d726bcce9b618ef1220c36050c039ddd84fa
WilsonHWS159 <n96101189@gs.ncku.edu.tw>
2022-04-11 14:26:49 +0800

    Simplify code Remove nonessential wire and reg.

--- HW1/ALU_1bit.v
+++ HW1/ALU_1bit.v
@@ -1,21 +1,21 @@
 module ALU_1bit(
 	input a, b, less, Ainvert, Binvert, c_in,
 	input [1:0] op,
-	output reg result, c_out, set, overflow);
+	output c_out, set, overflow,
+	output reg result);
 
-reg a1, b1;
-wire adder, out;
-FA FA_1(.s(adder), .carry_out(out), .x(a1), .y(b1), .carry_in(c_in));
+wire adder, out, a1, b1;
+
+assign a1 = (Ainvert) ? ~a : a;
+assign b1 = (Binvert) ? ~b : b;
+assign overflow = c_in ^ c_out;
+
+FA FA_1(.s(set), .carry_out(c_out), .x(a1), .y(b1), .carry_in(c_in));
 	always @(*) begin
-		a1 = (Ainvert) ? ~a : a;
-		b1 = (Binvert) ? ~b : b;
-		set = adder;
-		c_out = out;
-		overflow = c_in ^ out;
 		case (op)
 				2'b00:result = a1 & b1;
 				2'b01:result = a1 | b1; 
-				2'b10:result = adder;
+				2'b10:result = set;
 				2'b11:result = less;
 		endcase
 	end
--- HW1/ALU_8bit.v
+++ HW1/ALU_8bit.v
@@ -4,22 +4,19 @@ module ALU_8bit(
 	output zero, overflow,
 	output [7:0] result);
 
-wire [7:0] keep_result;
 wire [6:0] c_temp;
-wire less_temp, overflow_temp, set_temp;
-assign zero = ~|(keep_result);
-assign result = keep_result;
-assign overflow = overflow_temp;
-assign less_temp = set_temp ^ overflow_temp;
+wire less, set;
+assign zero = ~|result;
+assign less = set ^ overflow;
 generate
 	genvar i;
 	for(i=0;i<8;i=i+1) begin: alu
 		if(i == 0) begin
-			ALU_1bit ALU(.a(ALU_src1[i]), .b(ALU_src2[i]), .less(less_temp), .Ainvert(Ainvert), .Binvert(Binvert), .c_in(Binvert), .op(op), .result(keep_result[i]), .c_out(c_temp[i]), .set(), .overflow());
+			ALU_1bit ALU(.a(ALU_src1[i]), .b(ALU_src2[i]), .less(less), .Ainvert(Ainvert), .Binvert(Binvert), .c_in(Binvert), .op(op), .result(result[i]), .c_out(c_temp[i]), .set(), .overflow());
 		end else if(i == 7) begin
-			ALU_1bit ALU(.a(ALU_src1[i]), .b(ALU_src2[i]), .less(1'b0), .Ainvert(Ainvert), .Binvert(Binvert), .c_in(c_temp[i-1]), .op(op), .result(keep_result[i]), .c_out(), .set(set_temp), .overflow(overflow_temp));
+			ALU_1bit ALU(.a(ALU_src1[i]), .b(ALU_src2[i]), .less(1'b0), .Ainvert(Ainvert), .Binvert(Binvert), .c_in(c_temp[i-1]), .op(op), .result(result[i]), .c_out(), .set(set), .overflow(overflow));
 		end else begin
-			ALU_1bit ALU(.a(ALU_src1[i]), .b(ALU_src2[i]), .less(1'b0), .Ainvert(Ainvert), .Binvert(Binvert), .c_in(c_temp[i-1]), .op(op), .result(keep_result[i]), .c_out(c_temp[i]), .set(), .overflow());
+			ALU_1bit ALU(.a(ALU_src1[i]), .b(ALU_src2[i]), .less(1'b0), .Ainvert(Ainvert), .Binvert(Binvert), .c_in(c_temp[i-1]), .op(op), .result(result[i]), .c_out(c_temp[i]), .set(), .overflow());
 		end
 	end
 endgenerate

6ed849936300c0541438f32d9cdea22e5f39f68b
WilsonHWS159 <n96101189@gs.ncku.edu.tw>
2022-04-13 03:17:01 +0800

    Rewrite LZ77_Encoder.v Rewrite LZ77_Encoder.v The wave seems right but the testbench still can't receive data.

--- HW3/LZ77_Encoder.v
+++ HW3/LZ77_Encoder.v
@@ -1,109 +1,115 @@
 module LZ77_Encoder(clk,reset,chardata,valid,encode,finish,offset,match_len,char_nxt);
 
-
 input 				clk;
 input 				reset;
-input 		[7:0] 	chardata;
+input  [7:0]	 	chardata;
 output reg 			valid;
 output reg 			encode;
 output reg 			finish;
-output reg	[3:0] 	offset;
-output reg	[2:0] 	match_len;
-output reg 	[7:0] 	char_nxt;
+output reg [3:0]	offset;
+output reg [2:0]	match_len;
+output reg [7:0]	char_nxt;
 
-// 0~7 is look-ahead buffer, 8-16 is search buffer
-reg [7:0] buff[16:0];
-reg [3:0] hold;
-reg [16:0] buff_BM;
+	// 0~7 is look-ahead buffer, 8-16 is search buffer
+	reg [7:0] buff[16:0];
+	reg [3:0] hold;
+	reg [16:0] buff_BM;
+	reg done;
+	wire [7:0] cmp_result[8:0];
+	wire [3:0] cmp_len[8:0];
+	wire [3:0] max_len;
 
-always @(posedge reset) begin
-	buff_BM <= 17'h00000;
-	valid <= 1'b0;
-	encode <= 1'b1;
-	finish <= 1'b0;
-	offset <= 4'h0;
-	match_len <= 3'h0;
-	char_nxt <= 8'h00;
-end
-
-// data stream
-integer i;
-always @(posedge clk) begin
-	buff[0] <= chardata;
-	buff_BM[0] <= 1'b1;
-	for (i=0; i<16; i=i+1) begin
-		buff[i+1] <= buff[i];
-		buff_BM[i+1] <= buff_BM[i];
+	always @(posedge reset) begin
+		buff_BM <= 17'h00000;
+		encode <= 1'b1;
+		finish <= 1'b0;
 	end
-end
-
-wire [7:0] cmp_result[8:0];
-wire [4:0] cmp_len[8:0];
-wire [4:0] max_len;
-
-generate
-	genvar j;
-	for (j=0; j<9; j=j+1) begin: CMP
-		cmp Cmp(.buff1({buff[16-j], buff[15-j], buff[14-j], buff[13-j], buff[12-j], buff[11-j], buff[10-j], buff[9-j]}), .buff2({buff[7], buff[6], buff[5], buff[4], buff[3], buff[2], buff[1], buff[0]}), .result(cmp_result[8-j]), .len(cmp_len[8-j]));
-	end
-endgenerate
-
-find_max FM(.data_0(cmp_len[0]), .data_1(cmp_len[1]), .data_2(cmp_len[2]), .data_3(cmp_len[3]), .data_4(cmp_len[4]), .data_5(cmp_len[5]), .data_6(cmp_len[6]), .data_7(cmp_len[7]), .data_8(cmp_len[8]), .result(max_len));
-
-always @(posedge clk) begin
-	if (!hold & buff_BM[7]) begin
-		valid <= 1'b1;
-		hold <= max_len;
-		if (!max_len) begin
-			match_len <= max_len;
-			char_nxt <= buff[7];
-			offset <= 4'h0;
-		end else if (buff_BM[16] & (cmp_len[8] == max_len)) begin
-			match_len <= max_len;
-			char_nxt <= buff[7-max_len];
-			offset <= 4'h8;
-		end else if (buff_BM[15] & (cmp_len[7] == max_len)) begin
-			match_len <= max_len;
-			char_nxt <= buff[7-max_len];
-			offset <= 4'h7;
-		end else if (buff_BM[14] & (cmp_len[6] == max_len)) begin
-			match_len <= max_len;
-			char_nxt <= buff[7-max_len];
-			offset <= 4'h6;
-		end else if (buff_BM[13] & (cmp_len[5] == max_len)) begin
-			match_len <= max_len;
-			char_nxt <= buff[7-max_len];
-			offset <= 4'h5;
-		end else if (buff_BM[12] & (cmp_len[4] == max_len)) begin
-			match_len <= max_len;
-			char_nxt <= buff[7-max_len];
-			offset <= 4'h4;
-		end else if (buff_BM[11] & (cmp_len[3] == max_len)) begin
-			match_len <= max_len;
-			char_nxt <= buff[7-max_len];
-			offset <= 4'h3;
-		end else if (buff_BM[10] & (cmp_len[2] == max_len)) begin
-			match_len <= max_len;
-			char_nxt <= buff[7-max_len];
-			offset <= 4'h2;
-		end else if (buff_BM[9] & (cmp_len[1] == max_len)) begin
-			match_len <= max_len;
-			char_nxt <= buff[7-max_len];
-			offset <= 4'h1;
-		end else begin
-			match_len <= max_len;
-			char_nxt <= buff[7-max_len];
-			offset <= 4'h0;
+	
+	always @(posedge clk) begin
+		if (char_nxt == 8'h24) begin
+			encode <= 1'b0;
+			finish <= 1'b1;
 		end
-	end else if (~buff_BM[7]) begin
-		valid <= 1'b0;
-		offset <= 4'h0;
-		match_len <= 3'h0;
-		char_nxt <= 8'h00;
-		hold <= hold;
-	end else
-		hold <= hold - 1;
-end
+	end
+	
+	// data stream
+	integer i;
+	always @(posedge clk) begin
+		if (~reset) begin
+			buff[0] <= chardata;
+			buff_BM[0] <= 1'b1;
+			for (i=0;i<16;i=i+1) begin
+				buff[i+1] <= buff[i];
+				buff_BM[i+1] <= buff_BM[i];
+			end
+		end
+	end
+
+	generate
+		genvar j;
+		for (j=0; j<9; j=j+1) begin: CMP
+			cmp Cmp(.buff1({buff[16-j], buff[15-j], buff[14-j], buff[13-j], buff[12-j], buff[11-j], buff[10-j], buff[9-j]}), .buff2({buff[7], buff[6], buff[5], buff[4], buff[3], buff[2], buff[1], buff[0]}), .result(cmp_result[8-j]), .len(cmp_len[8-j]));
+		end
+	endgenerate
+	
+	
+	wire [3:0] tmp[6:0];
+	assign tmp[0] = buff_BM[9] ? ((cmp_len[0] > cmp_len[1]) ? cmp_len[0] : cmp_len[1]) : buff_BM[8] ? cmp_len[0] : 4'h0;
+	assign tmp[1] = buff_BM[11] ? ((cmp_len[2] > cmp_len[3]) ? cmp_len[2] : cmp_len[3]) : buff_BM[10] ? cmp_len[2] : 4'h0;
+	assign tmp[2] = buff_BM[13] ? ((cmp_len[4] > cmp_len[5]) ? cmp_len[4] : cmp_len[5]) : buff_BM[12] ? cmp_len[4] : 4'h0;
+	assign tmp[3] = buff_BM[15] ? ((cmp_len[6] > cmp_len[7]) ? cmp_len[6] : cmp_len[7]) : buff_BM[14] ? cmp_len[6] : 4'h0;
+	assign tmp[4] = (tmp[0] > tmp[1]) ? tmp[0] : tmp[1];
+	assign tmp[5] = (tmp[2] > tmp[3]) ? tmp[2] : tmp[3];
+	assign tmp[6] = (tmp[4] > tmp[5]) ? tmp[4] : tmp[5];
+	assign max_len = buff_BM[16] ? ((tmp[6] > cmp_len[8]) ? tmp[6] : cmp_len[8]) : tmp[6];
+	
+	// output
+	always @(*) begin
+		if (buff_BM[8]) begin
+			match_len = max_len;
+			char_nxt = buff[7-match_len];
+			if (!max_len)
+				offset <= 4'h0;
+			else if (cmp_len[8] == max_len)
+				offset <= 4'h8;
+			else if (cmp_len[7] == max_len)
+				offset <= 4'h7;
+			else if (cmp_len[6] == max_len)
+				offset <= 4'h6;
+			else if (cmp_len[5] == max_len)
+				offset <= 4'h5;
+			else if (cmp_len[4] == max_len)
+				offset <= 4'h4;
+			else if (cmp_len[3] == max_len)
+				offset <= 4'h3;
+			else if (cmp_len[2] == max_len)
+				offset <= 4'h2;
+			else if (cmp_len[1] == max_len)
+				offset <= 4'h1;
+		end else begin
+			match_len = 3'h0;
+			char_nxt = buff[7];
+			offset = 4'h0;
+		end
+	end
+	
+	always @(negedge clk) begin
+		if (buff_BM[7]) begin
+			if (!hold) begin
+				valid <= 1'b1;
+				encode <= 1'b0;
+				hold <= max_len;
+			end else begin
+				valid <= 1'b0;
+				encode <= 1'b1;
+				hold <= hold - 4'h0;
+			end
+		end else begin
+			hold <= 4'h0;
+			valid <= 1'b0;
+			encode <= 1'b1;
+		end
+	end
 
 endmodule
 
--- HW3/cmp.v
+++ HW3/cmp.v
@@ -2,7 +2,7 @@ module cmp(buff1, buff2, result, len);
 input		[63:0]	buff1;
 input 		[63:0]	buff2;
 output reg	[7:0]	result;
-output reg	[4:0]	len;
+output reg	[3:0]	len;
 
 always @(*) begin
 	result[0] = |(buff1[7:0] - buff2[7:0]);
@@ -17,15 +17,15 @@ end
 
 always @(*) begin
 	casez (result)
-		default: len = 5'h00;
-		8'b00000000: len = 5'h8;
-		8'b00000001: len = 5'h7;
-		8'b0000001z: len = 5'h6;
-		8'b000001zz: len = 5'h5;
-		8'b00001zzz: len = 5'h4;
-		8'b0001zzzz: len = 5'h3;
-		8'b001zzzzz: len = 5'h2;
-		8'b01zzzzzz: len = 5'h1;
+		default: len = 4'h00;
+		8'b00000000: len = 4'h8;
+		8'b00000001: len = 4'h7;
+		8'b0000001z: len = 4'h6;
+		8'b000001zz: len = 4'h5;
+		8'b00001zzz: len = 4'h4;
+		8'b0001zzzz: len = 4'h3;
+		8'b001zzzzz: len = 4'h2;
+		8'b01zzzzzz: len = 4'h1;
 	endcase
 end
 

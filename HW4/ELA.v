bc36897b151dd8e4375c2341ede38d1fe3c09095
WilsonHWS159 <n96101189@gs.ncku.edu.tw>
2022-05-17 16:32:45 +0800

    Upload HW4

---
+++ HW4/ELA.v
@@ -0,0 +1,21 @@
+`timescale 1ns/10ps
+
+module ELA(clk, rst, in_data, data_rd, req, wen, addr, data_wr, done);
+
+	input				clk;
+	input				rst;
+	input		[7:0]	in_data;
+	input		[7:0]	data_rd;
+	output				req;
+	output				wen;
+	output		[9:0]	addr;
+	output		[7:0]	data_wr;
+	output				done;
+
+
+	//--------------------------------------
+	//		Write your code here
+	//--------------------------------------
+
+
+endmodule

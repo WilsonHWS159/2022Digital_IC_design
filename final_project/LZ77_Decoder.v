1b8b1b239e0a77832193439ba28c9984cdc8ab83
WilsonHWS159 <n96101189@gs.ncku.edu.tw>
2022-06-17 20:49:27 +0800

    Implementation of final project decoder Refer to HW3 LZ77_Decoder

--- final_project/LZ77_Decoder.v
+++ final_project/LZ77_Decoder.v
@@ -6,14 +6,57 @@ input				ready;
 input 		[4:0] 	code_pos;
 input 		[4:0] 	code_len;
 input 		[7:0] 	chardata;
-output  			encode;
-output  			finish;
-output 	  [7:0] 	char_nxt;
+output reg 			encode;
+output reg 			finish;
+output reg	[7:0] 	char_nxt;
 
 
-	/*-------------------------------------/
-	/		Write your code here~		   /
-	/-------------------------------------*/
+	reg [7:0] buff[29:0];
+	reg [4:0] hold;
+	reg complete;
+	
+	integer i;
+	always @(posedge clk) begin
+		if (reset) begin
+			hold <= 5'h00;
+		end else if (finish) begin
+		end else if (ready) begin
+			if (!code_len) begin
+				buff[0] <= chardata;
+				char_nxt <= chardata;
+			end else if (!hold) begin
+				hold <= code_len;
+				buff[0] <= buff[code_pos];
+				char_nxt <= buff[code_pos];
+			end else if (hold == 5'h01) begin
+				hold <= 5'h00;
+				buff[0] <= chardata;
+				char_nxt <= chardata;
+			end else begin
+				hold <= hold - 5'h01;
+				buff[0] <= buff[code_pos];
+				char_nxt <= buff[code_pos];
+			end
+			for (i=0;i<29;i=i+1)
+				buff[i+1] <= buff[i];
+		end
+	end
+	
+	always @(posedge clk) begin
+		if (reset)
+			finish <= 1'b0;
+		else if (chardata == 8'h24)
+			if ((!hold & !code_len) | (hold == 5'h01))
+				finish <= 1'b1;
+			else
+				finish <= finish;
+		else
+			finish <= finish;
+	end
+	
+	always @(negedge reset) begin
+		encode <= 1'b0;
+	end
 
 
 endmodule
